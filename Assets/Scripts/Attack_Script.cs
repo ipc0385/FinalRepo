c383c2cab4743c8be21f852e2651911324835598
ipc0385 <ipc0385@louisiana.edu>
2015-11-25 19:58:24 -0600

    damage effects

--- Assets/Scripts/Attack_Script.cs
+++ Assets/Scripts/Attack_Script.cs
@@ -5,4 +5,12 @@ public class Attack_Script : MonoBehaviour
 {
 	public int myDamageValue;
 	public Effect_Script[] myDamageEffects, myTargetEffects;
+
+	public int damage
+	{
+		get
+		{
+			return Effect_Script.AffectsList(myDamageEffects, myDamageValue, gameObject, gameObject);
+		}
+	}
 }
--- Assets/Scripts/Health_Script.cs
+++ Assets/Scripts/Health_Script.cs
@@ -38,4 +38,21 @@ public class Health_Script : Effect_Script {
 		return health - damage;
 	}
 
+	public void Checkup ()
+	{
+		if(health < damage)
+		{
+			transform.parent = GetComponent<Card_Script>().myCardHolder.myOwner.myGraveyard.transform;
+		}
+	}
+
+	public void ApplyDamage(int input)
+	{
+		Term_Effect_Script termDamage = gameObject.AddComponent<Term_Effect_Script>();
+
+		termDamage.myTerm = -input;
+
+		Effect_Script.Append(myDamageEffects, termDamage);
+	}
+
 }
