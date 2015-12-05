16b9ca6ad5bfb92594862cefe3f91ecad0f7f4a5
oathkeeper005 <oathkeeper_rmm@hotmail.com>
2015-12-05 14:16:14 -0600

    Changes to Combat
    -Units will no longer kill Allied Units or the player that Owns them
    (How did I not think of this being an issue)
    -Red/Blue Tile Prefabs Now have an Owner Scripts (Please Set this in any
    scenes you use, Otherwise Units placed in the Back might kill their
    owner, also might be useful for placing units)

--- Assets/Scripts/CombatTurn.cs
+++ Assets/Scripts/CombatTurn.cs
@@ -6,6 +6,8 @@ public class CombatTurn : MonoBehaviour {
     
     private Attack_Script UnitAttack;
     private Health_Script UnitHealth;
+    private Owner_Script UnitOwner;
+
 
     private Cells UnitLocation;
     private Cells TilesInRange;
@@ -13,12 +15,14 @@ public class CombatTurn : MonoBehaviour {
     private GameObject enemyUnit;
     //private Attack_Script EnemyAttack;
     private Health_Script EnemyHealth;
+    private Owner_Script InRangeOwner;
 
     [SerializeField]
     private int range = 1;
 
 	// Use this for initialization
 	void Start () {
+        UnitOwner = gameObject.GetComponent<Owner_Script>();
         UnitAttack = gameObject.GetComponent<Attack_Script>();
         UnitHealth = gameObject.GetComponent<Health_Script>();
         UnitLocation = gameObject.GetComponentInParent<Cells>();
@@ -42,17 +46,25 @@ public class CombatTurn : MonoBehaviour {
                 TilesInRange = UnitLocation.GetForward(range).GetComponent<Cells>();
                 if (TilesInRange.isOccupied)
                 {
-                    EnemyHealth = TilesInRange.GetComponentInChildren<Health_Script>();
-                    EnemyHealth.ApplyDamage(UnitAttack.damage);
+                    InRangeOwner = TilesInRange.GetComponentInChildren<Owner_Script>();
+                    if (InRangeOwner.myOwner != UnitOwner.myOwner)
+                    {
+                        EnemyHealth = TilesInRange.GetComponentInChildren<Health_Script>();
+                        EnemyHealth.ApplyDamage(UnitAttack.damage);
+                    }
                 }
             }
         }
         else 
         {
-            //Debug.Log(gameObject.name + " has reached the end!");
-            EnemyHealth = GameObject.Find("EnemyPlayer").GetComponent<Health_Script>();
-            EnemyHealth.ApplyDamage(1);
-            UnitHealth.ApplyDamage(9001);
+            InRangeOwner = transform.parent.GetComponent<Owner_Script>();
+            if (InRangeOwner.myOwner != UnitOwner.myOwner)
+            {
+                //Debug.Log(gameObject.name + " has reached the end!");
+                EnemyHealth = GameObject.Find("EnemyPlayer").GetComponent<Health_Script>();
+                EnemyHealth.ApplyDamage(1);
+                UnitHealth.ApplyDamage(9001);
+            }
         }
     }
 }
--- Assets/Scripts/MoveUnits.cs
+++ Assets/Scripts/MoveUnits.cs
@@ -44,7 +44,39 @@ public class MoveUnits : MonoBehaviour {
                     }
                     else Debug.Log("Anouther Unit is Blocking the way of " + gameObject.name);
                 }
-                else Debug.Log(gameObject.name + " Cannot move forward");
+                else Debug.Log(gameObject.name + " Cannot move Player Unit Forward");
+            }
+            else Debug.Log(gameObject.name + " is stationary");
+        }
+        else Debug.Log(gameObject.name + " has reached the end!");
+    }
+
+    public void MoveEnemy()
+    {
+
+        if (transform.parent.GetComponent<Cells>().isEnd() == false)
+        {
+            //Unit = gameObject.GetComponent<UnitInfo>();
+            int distance = gameObject.GetComponent<UnitInfo>().GetSpeed();
+
+            if (distance > 0)
+            {
+                if (transform.parent.GetComponent<Cells>().BackExists(distance) == true)
+                {
+                    if (transform.parent.GetComponent<Cells>().GetBack(distance).GetComponent<Cells>().isOccupied == false)
+                    {
+
+                        // change parent
+                        GameObject Destination = transform.parent.GetComponent<Cells>().GetBack(distance);
+                        gameObject.transform.parent = Destination.transform;
+                        // now actually make it move
+                        movement.myAnim.AddClip(movement.MakeClip(1, Vector3.up.normalized * .35f), "moveUnit");
+                        movement.myAnim.Play("moveUnit");
+                        //transform.localPosition = Vector3.up.normalized*.35f;
+                    }
+                    else Debug.Log("Anouther Unit is Blocking the way of " + gameObject.name);
+                }
+                else Debug.Log(gameObject.name + " Cannot move Enemy Unit Forward");
             }
             else Debug.Log(gameObject.name + " is stationary");
         }
