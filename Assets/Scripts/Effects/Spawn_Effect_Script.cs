a0636f0dfcbc82678d5c643701313dd306ec27fc
oathkeeper005 <oathkeeper_rmm@hotmail.com>
2015-12-07 16:24:28 -0600

    Does it work
    It should

--- Assets/Scripts/Effects/Spawn_Effect_Script.cs
+++ Assets/Scripts/Effects/Spawn_Effect_Script.cs
@@ -6,11 +6,18 @@ public class Spawn_Effect_Script : Effect_Script
 	[SerializeField]
 	private GameObject mySpawn;
 
+    [SerializeField]
+    private Vector3 myInitialSize = Vector3.one;
+
 	public override int Affect(int initial, int input, GameObject inSubject, GameObject inObject)
 	{
 		GameObject spawn = Instantiate(mySpawn);
 
 		spawn.transform.parent = inObject.transform;
+
+        spawn.transform.localPosition = Vector3.up * 0.35f;
+
+        spawn.transform.localScale = myInitialSize;
 
 		return input;
 	}
